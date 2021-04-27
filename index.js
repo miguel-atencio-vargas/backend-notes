07716d4dd4a929a470b9e67997a16dc36a93cb08
skyfall947 <atencio947@gmail.com>
2021-04-27 18:41:48 -0400

    Fin de la seccion C.
    Guardando datos en MongoDB

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 'use strict';
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const express = require('express');
-const morgan = require('morgan');
 const cors = require('cors');
 
 const Note = require('./models/note');
 
 
 const app = express();
-app.use(express.json());
 app.use(express.static('build'))
 app.use(cors({ origin: 'http://localhost:3000' }));
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-morgan.token('body', req => JSON.stringify(req.body))
+app.use(express.json());
 
 
-app.get('/api/info', (req, res) => {
-  const HTMLdata = `
-    <p>Notes app have ${notes.length} notes.</p>
-    <p>${new Date()}</p>`;
-  res.send(HTMLdata)
-});
+const requestLogger = (req, res, next) => {
+  console.log('Method:', req.method);
+  console.log('Path:  ', req.path);
+  console.log('Body:  ', req.body);
+  console.log('---')
+  next();
+}
+app.use(requestLogger);
+
 
 app.get('/api/notes', (req, res) => {
   Note.find({}).then(notes => {
@@ -28,14 +28,22 @@ app.get('/api/notes', (req, res) => {
   });
 });
 
-app.get('/api/notes/:id', (req, res) => {
-  Note.findById(req.params.id).then(note => res.json(note));
+app.get('/api/notes/:id', (req, res, next) => {
+  Note.findById(req.params.id)
+    .then(note => {
+      if(!note) return res.status(404).end();
+      res.json(note);
+    })
+    .catch(err => next(err));
 });
 
-app.delete('/api/notes/:id', (req, res) => {
-  const id = +req.params.id;
-  notes = notes.filter(item => item.id !== id);
-  res.status(204).end();
+app.delete('/api/notes/:id', (req, res, next) => {
+  Note.findByIdAndRemove(req.params.id)
+    .then(result => {
+      if(!result) return res.status(404).send({message: 'ID not found'});
+      res.status(204).end();
+    })
+    .catch(err => next(err))
 });
 
 app.post('/api/notes', (req, res) => {
@@ -48,15 +56,36 @@ app.post('/api/notes', (req, res) => {
     important: body.important || false,
     date: new Date()
   });
-  note.save().then(savedNote => {
-    res.json(savedNote);
-  });
+  note.save().then(savedNote => res.json(savedNote));
+});
+
+app.put('/api/notes/:id', (req, res, next) => {
+  const body = req.body;
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+  Note.findByIdAndUpdate(req.params.id, note, {new: true})
+    .then( updatedNote => {
+      res.json(updatedNote)
+    })
+    .catch(err => next(err));
 });
 
 const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: 'unknown endpoint' });
 }
 app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error);
+  if(error.name === 'CastError') return res.status(400).send({ 
+    message: 'Malformatted ID'
+  });
+  next(error);
+}
+app.use(errorHandler);
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log('Server listen on:', PORT));
